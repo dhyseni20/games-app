eb03c3462483d8aa936af078ad9937a8cad3ccc3
Drilon Hyseni <emilianhyseni@gmail.com>
2023-07-28 23:33:53 +0200

    Like Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
+import Like from "./components/Like";
 
 function App() {
   const [alertVisible, setAlertVisible] = useState(false);
   return (
     <div>
+      <Like onClick={() => console.log("Clicked")} />
       {alertVisible && (
         <Alert onClose={() => setAlertVisible(false)}>My alert</Alert>
       )}
