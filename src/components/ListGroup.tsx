c579d865aeed8915cc46097f0d572e578d7b077f
Drilon Hyseni <emilianhyseni@gmail.com>
2023-07-28 18:14:53 +0200

    Handling Events

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,10 +1,13 @@
+import { MouseEvent } from "react";
 //Adding Fragments
 function ListGroup() {
-  let items = ["New York", "London", "San Francisco", "Paris", "Tokyo"];
+  const items = ["New York", "London", "San Francisco", "Paris", "Tokyo"];
 
   //items = [];
 
   //   const message = items.length === 0 ? <p> No items found</p> : null;
+
+  const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
     <>
@@ -13,8 +16,8 @@ function ListGroup() {
       {/* if the condition is true the element is shown */}
       {items.length === 0 && <p>No tiems found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item} className="list-group-item">
+        {items.map((item, index) => (
+          <li key={item} className="list-group-item" onClick={handleClick}>
             {item}
           </li>
         ))}
