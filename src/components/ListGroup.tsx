65e5c04567940751006b624bf18cbc4271e05863
Drilon Hyseni <emilianhyseni@gmail.com>
2023-07-28 17:59:05 +0200

    Conditional Rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,10 +1,17 @@
 //Adding Fragments
 function ListGroup() {
-  const items = ["New York", "London", "San Francisco", "Paris", "Tokyo"];
+  let items = ["New York", "London", "San Francisco", "Paris", "Tokyo"];
+
+  //items = [];
+
+  //   const message = items.length === 0 ? <p> No items found</p> : null;
 
   return (
     <>
       <h1>List</h1>
+      {/* {message} */}
+      {/* if the condition is true the element is shown */}
+      {items.length === 0 && <p>No tiems found</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item} className="list-group-item">
