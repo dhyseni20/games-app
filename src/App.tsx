9d07ec63108dad9797c50a373e8663dd668a0dd7
Drilon Hyseni <emilianhyseni@gmail.com>
2023-07-31 17:01:23 +0200

    Expense Filter 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,30 @@
 import { useState } from "react";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
+import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
 
 function App() {
+  const [selectedCategory, setCategory] = useState("");
   const [expenses, setExpenses] = useState([
     { id: 1, description: "aaa", amount: 10, category: "Utilities" },
-    { id: 2, description: "bbb", amount: 11, category: "Utilities" },
+    { id: 2, description: "bbb", amount: 11, category: "Groceries" },
     { id: 3, description: "nnn", amount: 3, category: "Utilities" },
     { id: 4, description: "ggg", amount: 45, category: "Utilities" },
   ]);
 
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((e) => e.category === selectedCategory)
+    : expenses;
+
   return (
     <div>
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectCategory={(category) => setCategory(category)}
+        ></ExpenseFilter>
+      </div>
+
       <ExpenseList
-        expenses={expenses}
+        expenses={visibleExpenses}
         onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
       ></ExpenseList>
     </div>
--- src/Message.tsx
+++
@@ -1,9 +0,0 @@
-//PascalCasing
-
-function Message() {
-  const name = "Drilon";
-  //JSX
-  return <h1>Hello {name}</h1>;
-}
-
-export default Message;
